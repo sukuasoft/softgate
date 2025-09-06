56e8bad2b42b8d75c76b38ade392ffaf062667cb
Sebastião Sukuakueche <sukuakueches@gmail.com>
2025-09-06 14:24:04 +0100

    update with images

--- script.js
+++ script.js
@@ -552,3 +552,17 @@ document.addEventListener('DOMContentLoaded', function() {
     
     console.log('Softgate website loaded successfully! 🚀');
 }); 
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 4000); // troca a cada 4s
+}
